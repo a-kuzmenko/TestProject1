4c15ffc0059291e428669a098da2743bece5c94f
Anna Kuzmenko <akuzmenko@cmlatitude.local>
2019-01-30 12:35:40 +0200

    Initial commit

---
+++ src/test/java/steps/RunFeatures.java
@@ -0,0 +1,13 @@
+package steps;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/java/features/")
+/**
+ * Created by akuzmenko on 9/15/2017.
+ */
+public class RunFeatures {
+}
